4b1d77a4c0193dea42ee634ede756147cf77f632
Sunny15611 <sunny84a3331@gmail.com>
2025-02-07 14:58:32 +0530

    Update ChatBox.tsx

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -140,7 +140,7 @@ export function ChatBox({ chatId }: { chatId?: string }) {
 
     startTransition(async () => {
       try {
-        const response = await fetch("https://24a7-103-147-138-252.ngrok-free.app/api/query1", {
+        const response = await fetch("https://2185-103-147-138-252.ngrok-free.app/api/query1", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
